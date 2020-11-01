8af2224edc285b7099235980edb5809db965b41d
Pablo LM <pabloplm@gmail.com>
2020-11-01 17:52:09 +0100

    Improvements

--- src/main/java/com/aeox/auth/exception/ApplicationExceptionMapper.java
+++ src/main/java/com/aeox/auth/exception/ApplicationExceptionMapper.java
@@ -2,12 +2,14 @@ package com.aeox.auth.exception;
 
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
 
 import com.aeox.auth.dto.ErrorMessage;
 
+@Provider
 public class ApplicationExceptionMapper implements ExceptionMapper<ApplicationException> {
     @Override
-    public Response toResponse(ApplicationException exception) {
+    public Response toResponse(final ApplicationException exception) {
         return Response.status(exception.getHttpStatus()).entity(new ErrorMessage(exception.getMessage())).build(); 
     }    
 }
