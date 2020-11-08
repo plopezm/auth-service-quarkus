1847bfdc29a0a5c9ba0f49648168991a418b6787
Pablo LM <pabloplm@gmail.com>
2020-11-08 13:52:56 +0100

    added scopes

--- src/main/java/com/aeox/auth/service/LoginService.java
+++ src/main/java/com/aeox/auth/service/LoginService.java
@@ -2,12 +2,19 @@ package com.aeox.auth.service;
 
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
+import java.util.LinkedList;
+import java.util.List;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.json.Json;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonObjectBuilder;
+import javax.transaction.Transactional;
 
 import com.aeox.auth.dto.login.LoginRequest;
 import com.aeox.auth.dto.login.LoginResponse;
 import com.aeox.auth.dto.signup.SignupRequest;
+import com.aeox.auth.entity.Scope;
 import com.aeox.auth.entity.User;
 import com.aeox.auth.exception.EntityNotFoundException;
 import com.aeox.auth.exception.LoginInvalidException;
@@ -37,24 +44,35 @@ public class LoginService {
         if (!user.getPassword().equals(User.hashPasswordUsingSalt(loginRequest.password, user.getSalt()))) {
             throw new LoginInvalidException();
         }
-        return new LoginResponse(this.generateToken(user));
+        return new LoginResponse(this.generateToken(user, user.getScopes()));
     }
     
+    @Transactional
     public LoginResponse signup(final SignupRequest request) {
         final User userRegistered = this.userService.create(request.getUserEntity());
+        final List<Scope> createdScopes = new LinkedList<>();
         if (request.hasScopes()) {
             request.getScopes().forEach(scopeRequest -> {
-                this.scopeService.create(userRegistered, scopeRequest);
+                createdScopes.add(this.scopeService.create(userRegistered, scopeRequest));
             });
         }
-        return new LoginResponse(this.generateToken(userRegistered));
+        return new LoginResponse(this.generateToken(userRegistered, createdScopes));
     }
 
-    private String generateToken(final User user) {
-        return Jwt.issuer("https://auth-service.aeox.com") 
-        .upn(user.getId().toString())
-        //.groups(new HashSet()<>(Arrays.asList("User", "Admin"))) 
+    private String generateToken(final User user, final List<Scope> scopes) {
+        final JsonArrayBuilder scopesBuilder = Json.createArrayBuilder();
+        scopes.stream().forEach(scope -> {
+            final JsonObjectBuilder scopeBuilder = Json.createObjectBuilder();
+            scopeBuilder.add("app", scope.getApplication().getName());
+            final JsonArrayBuilder rolesBuilder = Json.createArrayBuilder();
+            scope.getRoles().forEach(role -> rolesBuilder.add(role.getName()));
+            scopeBuilder.add("roles", rolesBuilder.build());
+            scopesBuilder.add(scopeBuilder.build());
+        });
+        return Jwt.issuer("https://auth-service.aeox.com")
+        .upn(user.getId().toString())        
         .claim(Claims.nickname.name(), user.getUsername())
+        .claim("scopes", scopesBuilder.build())
         .sign();
     }
 }
